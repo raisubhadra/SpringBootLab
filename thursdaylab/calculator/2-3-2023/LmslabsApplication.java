4631239baa7705a99644885fda3d117f60865a84
Subhadra <117344735+raisubhadra@users.noreply.github.com>
2023-03-03 23:14:12 +0530

    Add files via upload

---
+++ thursdaylab/calculator/2-3-2023/LmslabsApplication.java
@@ -0,0 +1,13 @@
+package lab.lmsassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmslabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmslabsApplication.class, args);
+	}
+
+}
